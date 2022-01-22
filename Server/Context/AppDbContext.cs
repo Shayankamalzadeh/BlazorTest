9bc080c716b3c93360c38e5ff69285a63d1b1b7b
Shayan <shayankamalzadeh@gmail.com>
2022-01-22 23:39:51 +0100

    Add Dataset User and Roles

---
+++ Server/Context/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using BlazorTest.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTest.Server.Context
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().
+                HasOne(u => u.Role).
+                WithMany(x => x.Users).
+                HasForeignKey(x => x.RoleId);
+        }
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+
+
+}
---
+++ Shared/Entities/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorTest.Shared.Entities
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Caption { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+    }
+
+}
